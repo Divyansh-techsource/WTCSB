806765d5c1e01a94e07f908dfc73e74ce1603a67
Divyansh-techsource <dk.tech2007@gmail.com>
2026-02-13 20:39:35 +0530

    13-02-2026

---
+++ practice.js
@@ -0,0 +1,31 @@
+// function vowelCount(str){
+//     let count=0;
+//     for(let i=0;i<str.length;i++){
+//         ch=str.charAt(i);
+//         if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||
+//             ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'
+//         )
+//         count++;
+//     }
+//     return count;
+// }
+// console.log(vowelCount("Anita"));
+
+// let str="Anita";
+// const vowelCount=(str)=>{
+//     let count=0;
+//     for(let i=0;i<str.length;i++){
+//         ch=str.charAt(i);
+//         if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||
+//             ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'
+//         )
+//         count++;
+//     }
+//     return count;
+// }
+// console.log(vowelCount(str));
+
+let arr=[1,2,3,4,5,6,7];
+arr.forEach((val)=>{
+    console.log(val*val);
+})
