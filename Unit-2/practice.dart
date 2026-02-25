14dd9e5f81c9c42d2cf523216b0aea228df2ab55
Divyansh-techsource <dk.tech2007@gmail.com>
2026-02-25 11:34:37 +0530

    25-02-2026

--- Unit-2/practice.dart
+++ Unit-2/practice.dart
@@ -114,7 +114,7 @@ class Student {
 // Objectives
 // 1. Exploring Inheritance
 
-void main() {
+/*void main() {
 
 	var dog = Dog();
 	dog.breed = "Labrador";
@@ -157,5 +157,51 @@ class Cat extends Animal {      // Cat is Child class or sub class, Animal is su
 
 	void meow() {
 		print("Meow !");
+	}
+}*/
+
+// Objectives
+// 1. Interface
+
+void main() {
+
+	var tv = Television();
+	tv.volumeUp();
+	tv.volumeDown();
+}
+
+class Remote {
+
+	void volumeUp() {
+		print("______Volume Up from Remote_______");
+	}
+
+	void volumeDown() {
+		print("______Volume Down from Remote_______");
+	}
+}
+
+class AnotherClass {
+
+	void justAnotherMethod(){
+		// Code
+	}
+
+}
+
+// Here Remote and AnotherClass acts as Interface
+class Television implements Remote, AnotherClass {
+
+	void volumeUp() {
+//		super.volumeUp();       // Not allowed to call super while implementing a class as Interface
+		print("______Volume Up in Television_______");
+	}
+
+	void volumeDown() {
+		print("______Volume Down in Television_______");
+	}
+
+	void justAnotherMethod() {
+		print("Some code");
 	}
 }
