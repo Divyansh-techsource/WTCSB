f2edb8608c9c438db2f61107cffbd5cca394da75
Divyansh-techsource <dk.tech2007@gmail.com>
2026-02-25 11:30:26 +0530

    dartessential

--- Unit-2/practice.dart
+++ Unit-2/practice.dart
@@ -37,7 +37,7 @@ class Student {
 // 3. Named Constructor
 // 4. Constant Constructor
 
-void main() {
+/*void main() {
 
 	var student1 = Student(23, "Peter"); 		// One Object, student1 is reference variable
 	print("${student1.id} and ${student1.name}");
@@ -82,4 +82,31 @@ class Student {
 	void sleep() {
 		print("${this.name} is now sleeping");
 	}
+}*/
+
+// Objectives
+// 1. Default Getter and Setter
+// 2. Custom Getter and Setter
+// 3. Private Instance Variable
+
+/*void main() {
+
+  var student = Student();
+  student.name = "Peter";     // Calling default Setter to set value
+  print(student.name);        // Calling default Getter to get value
+
+  student.percentage = 438.0;   // Calling Custom Setter to set value
+  print(student.percentage);    // Calling Custom Getter to get value
 }
+
+class Student {
+
+  String? name;        // Instance Variable with default Getter and Setter
+
+  double? _percent;    // Private Instance Variable for its own library
+
+  // Instance variable with Custom Setter
+  void set percentage(double marksSecured) => _percent = (marksSecured / 500) * 100;
+  // Instance variable with Custom Getter
+  double? get percentage => _percent;
+}*/
