190da10e420d8609648b652351558493e71024dd
Divyansh-techsource <dk.tech2007@gmail.com>
2026-03-17 20:44:35 +0530

    17-03-2026

--- Unit-1/index.js
+++ Unit-1/index.js
@@ -3,11 +3,11 @@
 // let a=20
 // console.log(a)
 
-const aa=()=>{return {user:"name"}};
-console.log(aa());
+// const aa=()=>{return {user:"name"}};
+// console.log(aa());
 
-const bb=()=>{return[1,2,3,4,5,6]};
-console.log(bb());
+// const bb=()=>{return[1,2,3,4,5,6]};
+// console.log(bb());
 /*
 const user={
     name: "aj", age:22,
